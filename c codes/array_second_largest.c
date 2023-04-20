57b7b6484a0f7c0bf13f3ac353e671370e48698d
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-04-20 12:51:28 +0530

    update
    update

--- c codes/array_second_largest.c
+++ c codes/array_second_largest.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-    int arr[6]={2,3,4,5,6,7};
+int arr[6]={2,3,4,5,6,7};
 int max=arr[0],smax=arr[0];
 for (int i = 0; i < 6; i++)
 {
---
+++ c codes/array_second_smallest.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements of the array:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int smallest = arr[0], second_smallest = arr[0];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < smallest) {
+            second_smallest = smallest;
+            smallest = arr[i];
+        } else if (arr[i] < second_smallest && arr[i] != smallest) {
+            second_smallest = arr[i];
+        }
+    }
+
+    printf("The second smallest element of the array is: %d\n", second_smallest);
+
+    return 0;
+}
