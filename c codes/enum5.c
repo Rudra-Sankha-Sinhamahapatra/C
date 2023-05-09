3f2dfa7281d04a07f6ae259d65c8378939a92aab
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2023-05-09 16:32:35 +0530

    update
    update

--- c codes/enum5.c
+++ c codes/enum5.c
@@ -1,23 +1,13 @@
 #include <stdio.h> 
-
 #include <stdlib.h> 
-
 struct cbits {
-
 unsigned b1:1;
-
 unsigned b2:1;
-
 unsigned b3:1;
-
 unsigned b4:1;
-
 unsigned b5:1;
-
 unsigned b6:1;
-
 unsigned b7:1;
-
 unsigned b8:1;
 };
 union U{
