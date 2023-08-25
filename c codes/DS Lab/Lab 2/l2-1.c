3c9bfe24934c5407b09d3b2433b7df47f05fee71
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-08-25 21:40:18 +0530

    updatd

---
+++ c codes/DS Lab/Lab 2/l2-1.c
@@ -0,0 +1,22 @@
+// 2.1 WAP to create a 1-D array of n elements and perform the following menu based operations using
+// function.
+// a. insert a given element at specific position.
+// b. delete an element from a specific position of the array.
+// c. linear search to search an element
+// d. traversal of the array
+// Sample Input/Output:
+// Enter size n : 5
+// Enter element of array:
+// Enter Array elements: 10 23 45 37 52
+// ***MENU***
+// 1. Insert
+// 2. Delete
+// 3. Linear Search
+// 4. Traverse
+// 5. Exit
+// Enter option: 1
+// Element to insert: 61 Enter Position: 2
+// Element inserted
+// Enter option: 4
+// Array Elements: 10 23 61 45 37 52
+// Note: Other menu choices are similarly needs to verify.
