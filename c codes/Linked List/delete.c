ab5b49fe1580d0dcdd6a39f8dd385becd714bab3
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-09-16 21:02:58 +0530

    updated

---
+++ c codes/Linked List/delete.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void printLinkedList(struct Node *head)
+{
+    struct Node *current = head;
+    while (current != NULL)
+    {
+        printf("%d ->", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+struct Node *deleteFromBeginning(struct Node *head)
+{
+    struct Node *ptr = head;
+    head = head->next;
+    free(ptr);
+    printf("Linked List after deleting the first node: ");
+    return head;
+};
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    // Allocating memory in heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    // storing data on first node
+    head->data = 7;
+    head->next = second;
+
+    // storing data on second node
+    second->data = 11;
+    second->next = third;
+
+    // storing data on third node
+    third->data = 31;
+    third->next = fourth;
+
+    // storing data on fourth node
+    fourth->data = 56;
+    fourth->next = NULL;
+
+    printf("Linked List: ");
+    printLinkedList(head);
+    // head=insertAtBeginning(head,24);
+    // head = insertAtIndex(head, 24, 2);
+    // head=insertAtEnd(head,24);
+    head = deleteFromBeginning(head);
+    printLinkedList(head);
+
+    free(head);
+    free(second);
+    free(third);
+    free(fourth);
+
+    return 0;
+}
