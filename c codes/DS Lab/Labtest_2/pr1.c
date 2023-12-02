0cb292c37305be9967dcd6d030a9c0216897889f
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-12-02 09:54:35 +0530

    updated

--- c codes/DS Lab/Labtest_2/pr1.c
+++ c codes/DS Lab/Labtest_2/pr1.c
@@ -1,59 +1,48 @@
 // Q1.Write C programme to sort a given no of elements using Quick Sort.
 
 #include <stdio.h>
-
-
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; 
-    int i = low - 1; 
+void swap(int *a, int *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+int partition (int arr[], int low, int high) {
+    int pivot = arr[high];
+    int i = (low - 1);
 
     for (int j = low; j <= high - 1; j++) {
-       
-        if (arr[j] <= pivot) {
+        if (arr[j] < pivot) {
             i++;
-          
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+            swap(&arr[i], &arr[j]);
         }
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
 }
-
-
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-
-        int pi = partition(arr, low, high);
-
-      
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int p = partition(arr, low, high);
+        quickSort(arr, low, p - 1);
+        quickSort(arr, p + 1, high);
     }
 }
-
-
 void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
+    int i;
+    for (i = 0; i < size; i++)
         printf("%d ", arr[i]);
     printf("\n");
 }
-
 int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: \n");
-    printArray(arr, n);
-
+   int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements: ");
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
     quickSort(arr, 0, n - 1);
-
     printf("Sorted array: \n");
     printArray(arr, n);
-
     return 0;
 }
