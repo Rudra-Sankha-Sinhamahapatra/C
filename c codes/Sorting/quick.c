3ad4935dd7859d979879f1d9175169b1642c2695
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2023-12-08 18:44:32 +0530

    updated

--- c codes/Sorting/quick.c
+++ c codes/Sorting/quick.c
@@ -1,59 +1,44 @@
-#include <stdio.h>
+#include<stdio.h>
 
-// Function to partition the array into two halves and return the pivot index
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high]; // Choose the rightmost element as the pivot
-    int i = low - 1; // Index of smaller element
-
-    for (int j = low; j <= high - 1; j++) {
-        // If the current element is smaller than or equal to the pivot
-        if (arr[j] <= pivot) {
-            i++;
-            // Swap arr[i] and arr[j]
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+void quicksort(int arr[],int low,int high){
+    if(low>=high){
+        return;
+    }
+    int s=low;
+    int e=high;
+    int m=s+(e-s)/2;
+    int pivot=arr[m];
+    while(s<=e){
+        while(arr[s]<pivot){
+            s++;
+        }
+        while(arr[e]>pivot){
+            e--;
+        }
+        if(s<=e){
+            int temp=arr[s];
+            arr[s]=arr[e];
+            arr[e]=temp;
+            s++;
+            e--;
         }
     }
-
-    // Swap arr[i + 1] and arr[high] (put the pivot in its correct position)
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-
-    return i + 1;
+    quicksort(arr,low,e);
+    quicksort(arr,s,high);
 }
 
-// Function to implement quicksort
-void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-        // Partition the array into two halves and get the pivot index
-        int pi = partition(arr, low, high);
-
-        // Recursively sort the subarrays before and after the pivot
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+int main(){
+    int n;
+    printf("Enter the size of the Array\n");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter the elements of the array\n");
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-}
-
-// Function to print an array
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    printf("Original array: \n");
-    printArray(arr, n);
-
-    quickSort(arr, 0, n - 1);
-
-    printf("Sorted array: \n");
-    printArray(arr, n);
-
-    return 0;
+    quicksort(arr,0,n-1);
+    printf("Sorted Array\n");
+    for(int i=0;i<n;i++){
+        printf("%d\t",arr[i]);
+    }
 }
