6076a8adf35387f42c0cc6dfaa6bdf293057a523
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-02-07 11:33:24 +0530

    changed

--- c codes/OSLab/fcfs.c
+++ c codes/OSLab/fcfs.c
@@ -1,70 +1,118 @@
-#include <stdio.h>  
-struct Process
-{
-     int pid;
-     int arival_time;
-     int burst_time;
-     int waiting_time;
-     int turn_around_time;
+// FCFS
+
+#include<stdio.h>
+#include<stdlib.h>
+
+struct process{
+    int pid;
+    int at;
+    int bt;
+    int wt;
+    int rt;
+    int tat;
 };
 
-struct Process process_list[10];
-// Function to calculate waiting time  
-void findWaitingTime( int processes[ ], int n, int bt[ ], int wt[ ])  
-{  
-    // waiting time for first process is 0  
-    wt[ 0 ] = 0;  
-  
-    // calculating waiting time  
-    for ( int i = 1 ; i < n ; i++ )  
-        wt[ i ] = bt[ i - 1 ] + wt[ i - 1 ] ;  
-}  
+struct process a[5];
 
-// Function to calculate turn around time  
-void findTurnAroundTime ( int processes[ ], int n, int bt[ ] , int wt [ ] , int tat[ ] )  
-{  
-    // calculating turnaround time by adding  
-    // bt [ i ] + wt [ i ]  
-    for ( int i = 0 ; i < n ; i++ )  
-        tat[i] = bt[i] + wt[i];  
-}  
-  
-// Function to calculate average time  
-void findavgTime(int processes[], int n, int bt[])  
-{  
-    int wt[n], tat[n], total_wt = 0, total_tat = 0;  
-  
-    findWaitingTime(processes, n, bt, wt);              // Function to find waiting time of all processes  
-  
-    findTurnAroundTime(processes, n, bt, wt, tat);      // Function to find turn around time for all processes
-  
-    // Display processes along with all details  
-    printf("\n\n%10s %20s %20s %20s","Processes", "Burst time", "Waiting time", "Turn around time");  
-    printf("\n=================================================================================");
-    
-    for (int i = 0; i < n; i++)     // Calculate total waiting time and total turn  around time
-    {  
-        total_wt = total_wt + wt[i];  
-        total_tat = total_tat + tat[i];  
-        printf("\n%10d", (i + 1));  
-        printf("%20d ", bt[i]);  
-        printf("%20d", wt[i]);  
-        printf("%20d", tat[i]);  
-    }  
-    int s = (float)total_wt / n;  
-    int t = (float)total_tat / n;  
-    printf("\n=================================================================================");
-    printf("\n\nAverage waiting time = %d", s);  
-    printf("\n\nAverage turn around time = %d ", t);  
-}  
- 
-int main()  
-{    
-    int processes[] = {1, 2, 3, 4, 5};    // process id's
-    int n = 5;  
-   
-    int burst_time[] = {20, 15, 28, 50, 30};  // Burst time of all processesN 
-  
-    findavgTime(processes, n, burst_time);  
-    return 0;  
-}  
+void bubbleSort(int n){
+    int i, j;
+    struct process temp;
+    for (i = 0; i < n - 1;i++){
+        for (j = 0; j < n - i - 1;j++){
+            if(a[j].at>a[j+1].at){
+                temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void avgwt(){
+    float avg = 0;
+    for (int i = 0; i < 5; i++){
+        avg += a[i].wt;
+    }
+    avg = avg / 5;
+    printf("\nAverage Waiting Time = %.2f", avg);
+}
+
+void avgtat(){
+    float avg = 0;
+    for (int i = 0; i < 5; i++){
+        avg += a[i].tat;
+    }
+    avg = avg / 5;
+    printf("\nAverage Turn Around Time = %.2f", avg);
+
+}
+
+void avgrt(){
+    float avg = 0;
+    for (int i = 0; i < 5; i++){
+        avg += a[i].rt;
+    }
+    avg = avg / 5;
+    printf("\nAverage Response Time = %.2f", avg);
+
+}
+
+void display(){
+    printf("\n\nProcess\t\tArrival Time\tBurst Time\tWaiting Time\tResponse Time\tTurn Around Time");
+    for (int i = 0; i < 5;i++){
+        printf("\nP%d \t\t%d \t\t%d \t\t%d \t\t%d \t\t%d", a[i].pid, a[i].at, a[i].bt, a[i].wt, a[i].rt, a[i].tat);
+    }
+
+    printf("\n");
+
+    avgwt();
+    avgrt();
+    avgtat();
+}
+
+int main(){
+    int i, j, sum_bt=0;
+
+    for (i = 0; i < 5; i++){
+        a[i].pid = i + 1;
+        printf("\nEnter P%d process Arrival Time: ", i+1);
+        scanf("%d", &a[i].at);
+        printf("\nEnter P%d process Burst Time: ", i+1);
+        scanf("%d", &a[i].bt);
+    }
+
+    bubbleSort(5);
+
+    // Waiting Time Calculation
+    for (i = 0; i < 5; i++){
+        if(i==0){
+            a[i].wt = 0;    
+        }
+        else{
+            a[i].wt = sum_bt - a[i].at;
+        }
+        sum_bt += a[i].bt;
+    }
+
+    // Turn Around Time Calculation
+    for (i = 0; i < 5; i++){
+        a[i].tat = a[i].wt + a[i].bt;
+    }
+
+    sum_bt = 0;
+
+    // Response Time Calculation
+    for (i = 0; i < 5;i++){
+        if(i==0){
+            a[i].rt = 0;     
+        }
+        else{
+            a[i].rt = sum_bt - a[i].at;
+        }
+        sum_bt += a[i].bt;
+    }
+
+    display();
+
+    return 0;
+}
