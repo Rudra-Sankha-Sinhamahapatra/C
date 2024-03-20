627688e5406c184b9c541678a21a5a9e18139dfc
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-03-21 02:02:42 +0530

    updated

---
+++ c codes/OSLab/producer_consumer.c
@@ -0,0 +1,73 @@
+#include<stdio.h>
+
+int mutex = 1;
+int empty;
+int full = 0;
+
+int n;
+int buffer[100];
+int front = 0;
+int rear = 0;
+
+void wait(int *s){
+    while(*s<=0);
+    (*s)--;
+}
+
+void signal(int *s){
+    (*s)++;
+}
+
+void producer(int item){
+    // while(1){
+        /* Item produced */
+        wait(&empty);
+        wait(&mutex);
+
+        buffer[rear] = item;
+        printf("\nProduced item is: %d", item);
+        rear = (rear+1)%n;
+
+        signal(&mutex);
+        signal(&full);
+    // }
+}
+
+void consumer(){
+    // while(1){
+        wait(&full);
+        wait(&mutex);
+
+        int item = buffer[front];
+        printf("\nConsumed item is: %d", item);
+        front = (front+1)%n;
+
+        signal(&mutex);
+        signal(&empty);
+        /* Item consumed */
+    // }
+}
+
+int main(){
+    printf("\nEnter n: ");
+    scanf("%d", &n);
+    empty = n;
+
+    producer(1);
+    printf("\nFull = %d", full);
+    printf("\nEmpty = %d", empty);
+    producer(2);
+    printf("\nFull = %d", full);
+    printf("\nEmpty = %d", empty);
+    producer(3);
+    printf("\nFull = %d", full);
+    printf("\nEmpty = %d", empty);
+    consumer();
+    printf("\nFull = %d", full);
+    printf("\nEmpty = %d", empty);
+    consumer();
+    printf("\nFull = %d", full);
+    printf("\nEmpty = %d", empty);
+
+    return 0;
+}
