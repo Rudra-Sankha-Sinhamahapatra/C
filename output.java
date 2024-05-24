2aef79af309e24946ff63e025720c9b9436e3778
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-05-24 13:40:28 +0530

    practiced java a bit

---
+++ output.java
@@ -0,0 +1,9 @@
+class output 
+{
+public static void main(String args[]) 
+     { 
+     StringBuffer s1 = new StringBuffer("Quiz"); 
+     StringBuffer s2 = s1.reverse();
+     System.out.println(s2);
+      } 
+  }
